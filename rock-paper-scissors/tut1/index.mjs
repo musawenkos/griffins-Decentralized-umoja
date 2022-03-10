fb2fb615673200a3f3ef6fdba53bddb6a4e0f2a6
https://github.com/musawenkos <ndlelamusa1st@gmail.com>
2022-03-10 05:26:30 +0200

    rps3

--- rock-paper-scissors/tut1/index.mjs
+++ rock-paper-scissors/tut1/index.mjs
@@ -7,6 +7,11 @@ const startingBalance = stdlib.parseCurrency(100);
 const accAlice = await stdlib.newTestAccount(startingBalance);
 const accBob = await stdlib.newTestAccount(startingBalance);
 
+const fmt = (x) => stdlib.formatCurrency(x,4);
+const getBalance = async (who) => fmt(await stdlib.balanceOf(who));
+const beforeAlice = await getBalance(accAlice);
+const beforeBob = await getBalance(accBob);
+
 const ctcAlice = accAlice.contract(backend);
 const ctcBob = accBob.contract(backend, ctcAlice.getInfo());
 
@@ -27,8 +32,19 @@ const Player = (Who) =>({
 await Promise.all([
     ctcAlice.p.Alice({
         ...Player('Alice'),
+        wager:stdlib.parseCurrency(5),
     }),
     ctcBob.p.Bob({
         ...Player('Bob'),
+        acceptWager: (amt) =>{
+            console.log(`Bob accepts the wager of ${fmt(amt)}.`);
+        }
     }),
 ]);
+
+const afterAlice = await getBalance(accAlice);
+const afterBob = await getBalance(accBob);
+
+
+console.log(`Alice went from ${beforeAlice} to ${afterAlice}`);
+console.log(`Bob went from ${beforeBob} to ${afterBob}`);
