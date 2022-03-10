47ca85308b169be21e0d408bf8f4b15ed459449c
https://github.com/musawenkos <ndlelamusa1st@gmail.com>
2022-03-10 04:57:36 +0200

    rps1 and rps2

---
+++ rock-paper-scissors/tut1/index.mjs
@@ -0,0 +1,34 @@
+import {loadStdlib} from '@reach-sh/stdlib';
+import * as backend from './build/index.main.mjs';
+
+const stdlib = loadStdlib();
+
+const startingBalance = stdlib.parseCurrency(100);
+const accAlice = await stdlib.newTestAccount(startingBalance);
+const accBob = await stdlib.newTestAccount(startingBalance);
+
+const ctcAlice = accAlice.contract(backend);
+const ctcBob = accBob.contract(backend, ctcAlice.getInfo());
+
+
+const HAND = [];
+const OUTCOME = [];
+const Player = (Who) =>({
+    getHand: () => {
+        const hand = Math.floor(Math.random() * 3);
+        console.log(`${Who} playes ${HAND[hand]}`);
+        return hand;
+    },
+    seeOutcome: (outcome) => {
+        console.log(`${Who} saw outcome ${OUTCOME[outcome]}`);
+    }
+});
+
+await Promise.all([
+    ctcAlice.p.Alice({
+        ...Player('Alice'),
+    }),
+    ctcBob.p.Bob({
+        ...Player('Bob'),
+    }),
+]);
