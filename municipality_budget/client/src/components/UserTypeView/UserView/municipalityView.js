751aa4def42bbf0864e844ad871ba961b37e24e8
https://github.com/musawenkos <ndlelamusa1st@gmail.com>
2022-03-22 21:14:47 +0200

    navbar

--- municipality_budget/client/src/components/UserTypeView/UserView/municipalityView.js
+++ municipality_budget/client/src/components/UserTypeView/UserView/municipalityView.js
@@ -1,7 +1,16 @@
 import React from 'react'
 import { MDBCol, MDBContainer, MDBRow, MDBInput, MDBBtn } from 'mdb-react-ui-kit'
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
 
 export default function MunicipalityView() {
+    const [age, setAge] = React.useState('');
+
+  const handleChange = (event) => {
+    setAge(event.target.value);
+  };
   return (
     <div>
         <MDBContainer>
@@ -9,29 +18,33 @@ export default function MunicipalityView() {
                 <MDBCol className='shadow-5'>
                     <MDBRow>
                         <MDBCol>
-                            <MDBInput label='Attach code' id='textAreaExample' textarea rows={4} />
+                            <MDBInput label='Attach code' id='textAreaExample' textarea rows={4} cols={5} />
                         </MDBCol>
                         <MDBCol>
                             <MDBBtn rounded>Attach</MDBBtn>
                         </MDBCol>
                     </MDBRow>
-                    <MDBRow>
-                        <div className='d-flex flex-row-reverse text-primary fs-5'>
-                            Balance of Money Received : 100 (ALGO)
-                        </div>
-                    </MDBRow>
                     <MDBRow className='mt-4'>
-                        <MDBCol>
+                        <MDBRow>
                             <MDBInput label='Amount(ALGO):' id='formControlDefault' type='text' />
-                        </MDBCol>
-                        <MDBCol>
-                            <select className="browser-default custom-select">
-                                <option>Select Local Suppliers</option>
-                                <option value="1">BME Group of Companies</option>
-                                <option value="2">Arrow Chem Chemical Manufacturers</option>
-                                <option value="3">The Cleaning Company Pty Ltd</option>
-                            </select>
-                        </MDBCol>
+                        </MDBRow>
+                        <MDBRow className='mt-2'>
+                            <FormControl variant="standard" sx={{ m: 1, minWidth: 120 }}>
+                                <InputLabel id="demo-simple-select-standard-label">Select List of Suppliers</InputLabel>
+                                <Select
+                                labelId="demo-simple-select-standard-label"
+                                id="demo-simple-select-standard"
+                                value={age}
+                                onChange={handleChange}
+                                label="Select List of Suppliers"
+                                >
+                                <MenuItem value={10}>BME Group of Companies</MenuItem>
+                                <MenuItem value={20}>Arrow Chem Chemical Manufacturers</MenuItem>
+                                <MenuItem value={30}>The Cleaning Company Pty Ltd</MenuItem>
+                                </Select>
+                            </FormControl>
+                           
+                        </MDBRow>
                     </MDBRow>
                     <MDBRow className='m-2'>
                         <div className='d-flex flex-row-reverse'>
@@ -41,6 +54,7 @@ export default function MunicipalityView() {
                 </MDBCol>
                 <MDBCol className='shadow-5'>
                     <div className='d-flex align-items-center justify-content-center mt-3'><h3 className='text-primary text-uppercase fw-bold'>Payment to Suppliers</h3></div>
+                    <div style={{height:"60vh"}}></div>
                 </MDBCol>
             </MDBRow>
         </MDBContainer>
--- municipality_budget/client/src/components/UserTypeView/UserView/treasuryView.js
+++ municipality_budget/client/src/components/UserTypeView/UserView/treasuryView.js
@@ -1,28 +1,45 @@
 import React from 'react'
 import { MDBCol, MDBContainer, MDBRow, MDBInput, MDBBtn } from 'mdb-react-ui-kit'
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+
 export default function TreasuryView(props) {
+    const [age, setAge] = React.useState('');
+
+  const handleChange = (event) => {
+    setAge(event.target.value);
+  };
   return (
     <div>
         <MDBContainer>
             <MDBRow >
                 <MDBCol className='shadow-5'>
-                    <MDBRow>
-                        <div className='d-flex flex-row-reverse text-primary fs-5'>
-                            Balance : 100 (ALGO)
-                        </div>
-                    </MDBRow>
-                    <MDBRow className='mt-4'>
-                        <MDBCol>
+                    
+                    <MDBRow className='mt-5'>
+                        <MDBRow>
                             <MDBInput label='Amount(ALGO):' id='formControlDefault' type='text' />
-                        </MDBCol>
-                        <MDBCol>
-                            <select className="browser-default custom-select">
-                                <option>Select Local Municipality</option>
-                                <option value="1">City of Cape Town Metropolitan Municipality</option>
-                                <option value="2">City of Ekurhuleni Metropolitan Municipality</option>
-                                <option value="3">City of Johannesburg Metropolitan Municipality</option>
-                            </select>
-                        </MDBCol>
+                        </MDBRow>
+                        <MDBRow className='mt-2'>
+                            <FormControl variant="standard" sx={{ m: 1, minWidth: 120 }}>
+                                <InputLabel id="demo-simple-select-standard-label">Select Local Municipality</InputLabel>
+                                <Select
+                                labelId="demo-simple-select-standard-label"
+                                id="demo-simple-select-standard"
+                                value={age}
+                                onChange={handleChange}
+                                label="Select Local Municipality"
+                                >
+                                <MenuItem value="">
+                                    <em>None</em>
+                                </MenuItem>
+                                <MenuItem value={10}>City of Cape Town Metropolitan Municipality</MenuItem>
+                                <MenuItem value={20}>City of Ekurhuleni Metropolitan Municipality</MenuItem>
+                                <MenuItem value={30}>City of Johannesburg Metropolitan Municipality</MenuItem>
+                                </Select>
+                            </FormControl>
+                        </MDBRow>
                     </MDBRow>
                     <MDBRow className='m-2'>
                         <div className='d-flex flex-row-reverse'>
@@ -32,7 +49,7 @@ export default function TreasuryView(props) {
                 </MDBCol>
                 <MDBCol className='shadow-5'>
                     <div className='d-flex align-items-center justify-content-center mt-3'><h3 className='text-primary text-uppercase fw-bold'>Payed Municipality</h3></div>
-                    
+                    <div style={{height:"65vh"}}></div>
                 </MDBCol>
             </MDBRow>
         </MDBContainer>
