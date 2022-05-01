b6cbe9a5f5945b7428ce0e9995b4e16a60644cfd
musawenkos <67800655+musawenkos@users.noreply.github.com>
2022-05-01 17:32:51 +0200

    Update firebase-config.js

--- municipality_budget/client/src/services/firebase-config.js
+++ municipality_budget/client/src/services/firebase-config.js
@@ -2,19 +2,7 @@ import {initializeApp} from "firebase/app";
 import {getFirestore} from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBcKRNPiU2r1_rpC4OJqKYdU1Wu9LPcQCo",
-
-    authDomain: "municipalitybc.firebaseapp.com",
-  
-    projectId: "municipalitybc",
-  
-    storageBucket: "municipalitybc.appspot.com",
-  
-    messagingSenderId: "696504928225",
-  
-    appId: "1:696504928225:web:f43cd8847b0b0f147734a0",
-  
-    measurementId: "G-5T291Z1HFQ"
+    
 };
 
 const app =  initializeApp(firebaseConfig);
